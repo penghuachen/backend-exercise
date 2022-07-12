af89c7bf4242ff01523409daaa6fa9332afdbe24
penghuachen <kids5346@gmail.com>
2022-07-12 12:23:22 +0800

    feat: prevent duplicate email

--- controllers/user.js
+++ controllers/user.js
@@ -6,11 +6,20 @@ const userController = {
     registerUser: async (req, res) => {
         const { name, email, password } = req.body;
 
+        const isExisted = await Users.findOne({
+            where: { email }
+        });
+
+        if (isExisted) {
+            res.status(422).send('email has already existed!');
+            return;
+        }
+
         await Users.create({
             name,
             email,
             password: hashPassword(password),
-        })
+        });
 
         res.send('register successfully!');
     },
