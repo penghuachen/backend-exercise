fbdb579834c7a95b9d70f462014dc104753d86d6
penghuachen <kids5346@gmail.com>
2022-07-13 13:52:20 +0800

    feat: update migration file

--- migrations/20220712012009-create-users.js
+++ migrations/20220712012009-create-users.js
@@ -9,12 +9,15 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       email: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       password: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
