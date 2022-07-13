3375b61fe292a5a86f987e1bd56f42378a99c3d5
penghuachen <kids5346@gmail.com>
2022-07-13 13:52:29 +0800

    feat: validate fields

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,27 @@
 const db = require('../models');
+const Joi = require('joi');
 const hashPassword = require("../utils/hashPassword.js");
+const validateFields = require("../utils/validateFields.js");
 const Users = db.Users;
 
 const userController = {
     registerUser: async (req, res) => {
         const { name, email, password } = req.body;
 
+        const schema = Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required(),
+            password: Joi.string().required(),
+        });
+
+        // fields validate
+        const fieldsErrors = validateFields(schema, req.body);
+        if (fieldsErrors) {
+            res.status(422).send(fieldsErrors);
+            return;
+        }
+
+        // business logic validate
         const isExisted = await Users.findOne({
             where: { email }
         });
