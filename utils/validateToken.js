276505b15ef1cb870428db3a21265d389d486936
penghuachen <kids5346@gmail.com>
2022-07-19 17:28:17 +0800

    fix: return boolean when valid token

--- utils/validateToken.js
+++ utils/validateToken.js
@@ -4,8 +4,10 @@ const vaidateToken = (req, res) => {
   const [prefix, token] = req.headers["authorization"].split(" ");
   try {
     jwt.verify(token, process.env.jwt_secret_key);
+    return true;
   } catch (err) {
     res.status(401).send('unauthorized');
+    return false;
   }
 };
 
