91a8c64a07f4da0cfc67405b033e6092133facf3
penghuachen <kids5346@gmail.com>
2022-07-19 16:33:08 +0800

    feat: add queries in order to filter more condition data

--- utils/getMatchedData.js
+++ utils/getMatchedData.js
@@ -1,7 +1,10 @@
-const getMatchedData = async ({ db, page, size }) => {
+const getMatchedData = async ({ db, page, size, queries = {} }) => {
   if (page < 1 || size < 1) return { data: [] };
 
   const { rows } = await db.findAndCountAll({
+    where: {
+      ...queries
+    },
     limit: +size, // 每頁幾個
     offset: (page - 1) * size // 跳過幾個
   });
