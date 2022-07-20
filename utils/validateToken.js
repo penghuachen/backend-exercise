937770817c77586dbe6948168022b88986aa5e9b
penghuachen <kids5346@gmail.com>
2022-07-20 15:27:30 +0800

    fix: return decode value

--- utils/validateToken.js
+++ utils/validateToken.js
@@ -3,8 +3,7 @@ const jwt = require('jsonwebtoken');
 const vaidateToken = (req, res) => {
   const [prefix, token] = req.headers["authorization"].split(" ");
   try {
-    jwt.verify(token, process.env.jwt_secret_key);
-    return true;
+    return jwt.verify(token, process.env.jwt_secret_key);
   } catch (err) {
     res.status(401).send('unauthorized');
     return false;
